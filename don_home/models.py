8664e73875fbbba17c6fdb921af9ef06ce642d14
sbzkdl <sbfpdl@naver.com>
2023-01-30 18:15:42 +0900

    cafe24_model

--- don_home/models.py
+++ don_home/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-    pass
+class Cafe24(models.Model):
+    cafe24_id = models.CharField('아이디', max_length=50, null=False)
+    cafe24_pw = models.CharField('비밀번호', max_length=50, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    
+
     
